3b5ff446291d91a3ef8f2ee0d24fb0859b3f7a81
Ben Robertson <hi@benrobertson.io>
2021-09-22 17:44:42 -0400

    updates.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,42 +1,60 @@
 const Promise = require('bluebird')
 const path = require('path')
 
-exports.createPages = ({ graphql, actions }) => {
+
+// Add a reporter to see how long it takes to generate blog posts.
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
-  return new Promise((resolve, reject) => {
-    const blogPost = path.resolve('./src/templates/blog-post.js')
-    resolve(
-      graphql(
-        `
-          {
-            allContentfulBlogPost {
-              edges {
-                node {
-                  title
-                  slug
-                }
-              }
+  // Create blog posts
+  const blogPost = path.resolve('./src/templates/blog-post.js')
+
+  let result = await graphql(
+    `
+      {
+        allContentfulBlogPost {
+          edges {
+            node {
+              title
+              slug
             }
           }
-        `
-      ).then(result => {
-        if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
         }
+      }
+    `
+  )
 
-        const posts = result.data.allContentfulBlogPost.edges
-        posts.forEach(post => {
-          createPage({
-            path: `/blog/${post.node.slug}/`,
-            component: blogPost,
-            context: {
-              slug: post.node.slug,
-            },
-          })
-        })
-      })
-    )
+  if (result.errors) {
+    console.log(result.errors)
+    reject(result.errors)
+  }
+
+  const posts = result.data.allContentfulBlogPost.edges
+  posts.forEach(post => {
+    createPage({
+      path: `/blog/${post.node.slug}/`,
+      component: blogPost,
+      context: {
+        slug: post.node.slug,
+      },
+    })
   })
+
+  // Create arbitrary pages
+  for (let index = 0; index < 10000; index++) {
+    createPage({
+      path: `/test/${index}`,
+      component: path.resolve(`./src/templates/test-template.js`),
+      context: {
+        page: index
+      }
+    })
+
+  }
+}
+
+
+exports.onPostBuild = () => {
+
+
 }
---
+++ src/templates/test-template.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const TestTemplate = ({ pageContext }) => (
+  <h1>Test Page {pageContext.page}</h1>
+);
+
+
+export default TestTemplate;
